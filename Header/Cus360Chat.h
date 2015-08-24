3ce8b9f5ae4c9d8ac0ed51995f7dab93ac8a9e09
Atul Limje <customer360@customer360s-Mac-mini.local>
2015-08-24 10:52:30 +0530

    New Inapp chat design rollout

---
+++ Header/Cus360Chat.h
@@ -0,0 +1,14 @@
+//
+//  Cus360Chat.h
+//  Cus360Chat
+//
+//  Created by Anveshan Technologies on 25/03/15.
+//
+
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cus360Chat : NSObject
+
+@end
--- README.md
+++
@@ -1,5 +0,0 @@
-Customer360-iOS-Chat-SDK
-=======================
-
-Offical Repo For Customer360 iOS chat SDK
-In order to get started , download the zip and import the proects into your workspace
